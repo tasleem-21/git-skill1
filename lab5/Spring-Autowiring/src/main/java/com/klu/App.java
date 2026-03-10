482303746f4b889d9d0625268981b1699bdce63b
tasleem-21 <2400030282@kluniversity.in>
2026-03-10 08:30:12 +0530

    Added lab4 and lab5

---
+++ lab5/Spring-Autowiring/src/main/java/com/klu/App.java
@@ -0,0 +1,18 @@
+package com.klu;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Student student = context.getBean(Student.class);
+        student.display();
+
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
